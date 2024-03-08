52c4c25892a3c60ec382f59e18aa5ab208e3ba12
edayot <pro.e.dayot@gmail.com>
2024-03-08 09:56:56 +0100

    style: running black

--- display_image.py
+++ display_image.py
@@ -13,7 +13,6 @@ import shutil
 import pokebase as pb
 
 
-
 from utils import crop_null_rectangle, add_transparency_border, add_empty_line
 
 DOUBLE_PIXEL = "▀"
@@ -23,7 +22,7 @@ BACKGROUND_COLOR = rich.color.Color.from_rgb(0, 0, 0)
 
 
 class ImageDisplay:
-    def __init__(self, img : Image.Image, display_background=True):
+    def __init__(self, img: Image.Image, display_background=True):
         self.console = rich.console.Console()
         img = img.convert("RGBA")
         img = crop_null_rectangle(img)
@@ -31,7 +30,7 @@ class ImageDisplay:
         img = add_empty_line(img)
         self.img = img
         self.display_background = display_background
-    
+
     def __rich_console__(self, console, options):
         for y in range(0, self.img.height, 2):
             for x in range(self.img.width):
@@ -41,24 +40,40 @@ class ImageDisplay:
                 bottom_color = rich.color.Color.from_rgb(*p2[:3])
 
                 if self.display_background:
-                    yield Segment(DOUBLE_PIXEL, style=rich.style.Style(color=top_color, bgcolor=bottom_color))
-                elif not top_color == BACKGROUND_COLOR and not bottom_color == BACKGROUND_COLOR:
-                    yield Segment(DOUBLE_PIXEL, style=rich.style.Style(color=top_color, bgcolor=bottom_color))
-                elif top_color == BACKGROUND_COLOR and not bottom_color == BACKGROUND_COLOR:
-                    yield Segment(DOUBLE_PIXEL_REVERSE, style=rich.style.Style(color=bottom_color))
-                elif not top_color == BACKGROUND_COLOR and bottom_color == BACKGROUND_COLOR:
+                    yield Segment(
+                        DOUBLE_PIXEL,
+                        style=rich.style.Style(color=top_color, bgcolor=bottom_color),
+                    )
+                elif (
+                    not top_color == BACKGROUND_COLOR
+                    and not bottom_color == BACKGROUND_COLOR
+                ):
+                    yield Segment(
+                        DOUBLE_PIXEL,
+                        style=rich.style.Style(color=top_color, bgcolor=bottom_color),
+                    )
+                elif (
+                    top_color == BACKGROUND_COLOR
+                    and not bottom_color == BACKGROUND_COLOR
+                ):
+                    yield Segment(
+                        DOUBLE_PIXEL_REVERSE, style=rich.style.Style(color=bottom_color)
+                    )
+                elif (
+                    not top_color == BACKGROUND_COLOR
+                    and bottom_color == BACKGROUND_COLOR
+                ):
                     yield Segment(DOUBLE_PIXEL, style=rich.style.Style(color=top_color))
                 else:
                     yield Segment(" ")
             yield Segment.line()
-    
 
 
 def sort_by_version(x):
     return x.version.url.split("/")[-2]
 
 
-def get_pokemon_info(pokemon_id, language='en'):
+def get_pokemon_info(pokemon_id, language="en"):
     pokemon_info = pb.pokemon(pokemon_id)
     pokemon_species = pb.pokemon_species(pokemon_id)
     pokemon_sprites = pb.SpriteResource("pokemon", pokemon_id)
@@ -71,13 +86,12 @@ def get_pokemon_info(pokemon_id, language='en'):
     # fallback to english if not found
     names_lang = [n.name for n in names if n.language.name == language]
     if len(names_lang) == 0:
-        names_lang = [n.name for n in names if n.language.name == 'en']
+        names_lang = [n.name for n in names if n.language.name == "en"]
     name = names_lang[0]
-
 
     descriptions_lang = [d for d in descriptions if d.language.name == language]
     if len(descriptions_lang) == 0:
-        descriptions_lang = [d for d in descriptions if d.language.name == 'en']
+        descriptions_lang = [d for d in descriptions if d.language.name == "en"]
     descriptions_lang.sort(key=lambda x: sort_by_version(x))
     description = descriptions_lang[0].flavor_text
 
@@ -88,28 +102,25 @@ def get_pokemon_info(pokemon_id, language='en'):
         names = type_data.names
         names_lang = [n.name for n in names if n.language.name == language]
         if len(names_lang) == 0:
-            names_lang = [n.name for n in names if n.language.name == 'en']
+            names_lang = [n.name for n in names if n.language.name == "en"]
         type_name = names_lang[0]
         l_types.append(type_name)
-        
-
 
     return {
-        'name': name,
-        'description': description,
-        'img': img,
+        "name": name,
+        "description": description,
+        "img": img,
         "types": l_types,
     }
 
 
 def generate_types_panel(types: list[str]):
     table = Table.grid(padding=1, expand=True)
-    table.add_column(justify="center",vertical="middle")
+    table.add_column(justify="center", vertical="middle")
     table.add_row("")
     for t in types:
         table.add_row(f"[{t}]")
     return Panel(table, border_style="blue")
-
 
 
 def display_pokemon(pokemon_id, language, display_background):
@@ -117,37 +128,56 @@ def display_pokemon(pokemon_id, language, display_background):
 
     pokemon_info = get_pokemon_info(pokemon_id, language)
 
-    image_display = ImageDisplay(pokemon_info['img'], display_background)
+    image_display = ImageDisplay(pokemon_info["img"], display_background)
     description = Markdown(
-f"""# {pokemon_info['name']} (#{pokemon_id})
+        f"""# {pokemon_info['name']} (#{pokemon_id})
 
 {pokemon_info['description']}
 
 
-""".strip("\n").strip()
+""".strip(
+            "\n"
+        ).strip()
     )
 
     layout = Layout(name="root")
     layout.split_row(
         Layout(Panel(image_display), name="image", size=image_display.img.width + 4),
-        Layout(name="desc", size=(35))
+        Layout(name="desc", size=(35)),
     )
     layout["desc"].split_column(
         Layout(Panel(description, border_style="green"), name="description"),
-        generate_types_panel(pokemon_info["types"])
+        generate_types_panel(pokemon_info["types"]),
     )
 
     _width, _height = shutil.get_terminal_size()
 
-    _height = image_display.img.height//2 + 2
+    _height = image_display.img.height // 2 + 2
     console.size = (_width, _height)
     console.print(layout)
 
 
 @click.command(name="pokeinfo")
-@click.option('--language', '-l', default='en', help='Language to display the pokemon name / description')
-@click.option('--pokemon_id', '-p', default=None, multiple=True, type=int, help='Pokemon ids to display, can be multiple.')
-@click.option('--display-background', '-d', is_flag=True, help='Display the background of the image as black pixels.')
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language to display the pokemon name / description",
+)
+@click.option(
+    "--pokemon_id",
+    "-p",
+    default=None,
+    multiple=True,
+    type=int,
+    help="Pokemon ids to display, can be multiple.",
+)
+@click.option(
+    "--display-background",
+    "-d",
+    is_flag=True,
+    help="Display the background of the image as black pixels.",
+)
 def main(language, pokemon_id, display_background):
     if pokemon_id:
         for p in pokemon_id:
@@ -156,6 +186,7 @@ def main(language, pokemon_id, display_background):
         print(":cross_mark: [dark_red]No pokemon id provided[/dark_red]")
         # display the help
         print(main.get_help(click.Context(main)))
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,25 +1,19 @@
-
 from PIL import Image
-
-
-
-
-
 
 
 def crop_null_rectangle(image):
     # Convert image to grayscale
-    grayscale_image = image.convert('L')
-    
+    grayscale_image = image.convert("L")
+
     # Get image size
     width, height = grayscale_image.size
-    
+
     # Find bounding box of non-null pixels
     left = width
     top = height
     right = 0
     bottom = 0
-    
+
     for x in range(width):
         for y in range(height):
             pixel_value = grayscale_image.getpixel((x, y))
@@ -28,7 +22,7 @@ def crop_null_rectangle(image):
                 top = min(top, y)
                 right = max(right, x)
                 bottom = max(bottom, y)
-    
+
     # Crop the image based on bounding box
     if left < right and top < bottom:
         cropped_image = image.crop((left, top, right + 1, bottom + 1))
@@ -42,30 +36,38 @@ def add_transparency_border(image, border_size):
     width, height = image.size
     new_width = width + 2 * border_size
     new_height = height + 2 * border_size
-    
+
     # Create a new image with larger dimensions and transparent background
     new_image = Image.new("RGBA", (new_width, new_height), (0, 0, 0, 0))
-    
+
     # Paste the cropped image onto the new image with an offset to add the border
     offset = (border_size, border_size)
     new_image.paste(image, offset)
-    
+
     # Draw a rectangle around the pasted image with a specified border color
-    border_color = (0, 0, 0, 128)  # Example: RGBA color with 50% opacity (adjust as needed)
+    border_color = (
+        0,
+        0,
+        0,
+        128,
+    )  # Example: RGBA color with 50% opacity (adjust as needed)
     border_rectangle = Image.new("RGBA", (new_width, new_height), border_color)
     border_rectangle.paste(new_image, (0, 0), mask=new_image)
-    
+
     return border_rectangle
+
 
 def add_empty_line(image):
     width, height = image.size
-    
+
     # Check if height is not a multiple of 2
     if height % 2 != 0:
         # Create a new image with an extra line at the top
         new_height = height + 1
         new_image = Image.new("RGBA", (width, new_height), (0, 0, 0, 0))
-        new_image.paste(image, (0, 1))  # Paste the original image with an offset to leave an empty line at the top
+        new_image.paste(
+            image, (0, 1)
+        )  # Paste the original image with an offset to leave an empty line at the top
         return new_image
     else:
         return image
